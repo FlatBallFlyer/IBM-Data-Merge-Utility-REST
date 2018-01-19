0f506b618550e706f64ea7f477b59eb379571061
Mike Storey <mike@ssgpp.com>
2018-01-19 13:34:03 -0500

    Shifted to Tomcat
    Liberty startup delays are awful

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -1,7 +1,7 @@
 cp /Users/flatballflyer/GitHub/IBM-Data-Merge-Utility-REST/target/idmu-rest-4.0.1-SNAPSHOT.war ./war/
-docker container stop idmu
-docker container rm idmu
+docker stop idmu
+docker rm idmu
 docker rmi idmu
 docker build -t idmu .
-docker run -d -p 9080:9080 -p 9443:9443 --name idmu idmu
+docker run -d -p 9080:8080 -p 9443:9443 --name idmu idmu
 
