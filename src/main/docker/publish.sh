eabd76c6dc1ace8a5bb802a9d7f0504d767cdd78
Mike Storey <mike@ssgpp.com>
2018-01-19 13:52:14 -0500

    updated for Tomcat

--- src/main/docker/publish.sh
+++ src/main/docker/publish.sh
@@ -1,3 +1,4 @@
 docker tag idmu flatballflyer/idmu:latest
 docker push flatballflyer/idmu:latest
-docker run -d -p 9080:9080 -p 9443:9443 --name idmu flatballflyer/idmu:latest
+docker run -d -p 9080:8080 --name idmu flatballflyer/idmu:latest
+
