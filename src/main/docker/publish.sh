905ba08626e32f0213a5a6c18c16672585e86bd1
Mike Storey <mike@ssgpp.com>
2018-10-06 12:23:53 -0400

    Removed Tomcat Admin config
    Removed the tomcat admin portal.

--- src/main/docker/publish.sh
+++ src/main/docker/publish.sh
@@ -1,4 +1,6 @@
 docker tag idmu flatballflyer/idmu:latest
 docker push flatballflyer/idmu:latest
+docker stop idmu
+docker rm idmu
 docker run -d -p 9080:8080 --name idmu flatballflyer/idmu:latest
 
