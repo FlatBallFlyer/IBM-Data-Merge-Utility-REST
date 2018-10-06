6d34efed0ea98e335c686f0202e361b572fe9b7b
Mike Storey <mike@ssgpp.com>
2018-10-06 14:06:09 -0400

    Version Update

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -1,7 +1,9 @@
-cp /Users/flatballflyer/GitHub/IBM-Data-Merge-Utility-REST/target/idmu-rest-4.0.1-SNAPSHOT.war ./war/
+cp /Users/flatballflyer/GitHub/IBM-Data-Merge-Utility-REST/target/idmu-rest-4.0.2.war ./war/
 docker stop idmu
 docker rm idmu
 docker rmi idmu
 docker build -t idmu .
+docker tag idmu flatballflyer/idmu:latest
+docker tag idmu flatballflyer/idmu:4.0.2
 docker run -d -p 9080:8080 -p 9443:9443 --name idmu idmu
 
